176dc7574bdb1f9d995553419ee4b11c688a4497
VolshVs <89104047740@mail.ru>
2023-12-11 22:33:51 +0300

    Adjustment INSERT-Script.sql and SELECT-Script.sql

--- SELECT-Script.sql
+++ SELECT-Script.sql
@@ -5,37 +5,32 @@ WHERE duration = (SELECT MAX(duration) FROM Track);
 
 --Название треков, продолжительность которых не менее 3,5 минут.
 SELECT track_title, duration FROM Track
-WHERE duration > 210;
+WHERE duration >= 210;
 
 --Названия сборников, вышедших в период с 2018 по 2020 год включительно.
 SELECT collection_title FROM collection
-WHERE year_of_realease = 2018 OR year_of_realease = 2019 OR year_of_realease = 2020; 
+WHERE year_of_realease BETWEEN 2018 AND 2020; 
 
 --Исполнители, чьё имя состоит из одного слова.
 SELECT name_pseudonym FROM Artist
-WHERE name_pseudonym <> (SELECT name_pseudonym FROM Artist WHERE name_pseudonym LIKE '% %');
+WHERE name_pseudonym NOT LIKE '% %';
 
 --Название треков, которые содержат слово «мой» или «my».
 SELECT track_title FROM Track
-WHERE track_title LIKE '%Мой%';
+WHERE track_title ILIKE 'my %'
+OR track_title ILIKE '% my'
+OR track_title ILIKE '% my %'
+OR track_title ILIKE 'my';
 
---Задание 3
 --Количество исполнителей в каждом жанре.
 SELECT genre_title, COUNT(artist_id) FROM Genre AS g
 JOIN Genre_Artist AS ga ON g.genre_id = ga.genre_id
 GROUP BY genre_title;
 
 --Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT track_title, year_of_release, COUNT(year_of_release) FROM Track AS t
+SELECT COUNT(year_of_release) FROM Track AS t
 JOIN Album AS a ON t.album_id = a.album_id
-WHERE year_of_release = 2019 OR year_of_release = 2020
-GROUP BY track_title, year_of_release;
-
---Или так (короткая версия) - Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT year_of_release, COUNT(year_of_release) FROM Track AS t
-JOIN Album AS a ON t.album_id = a.album_id
-WHERE year_of_release = 2019 OR year_of_release = 2020
-GROUP BY year_of_release;
+WHERE year_of_release BETWEEN 2019 AND 2020;
 
 --Средняя продолжительность треков по каждому альбому.
 SELECT album_title, AVG(duration) FROM Album AS a
@@ -43,12 +38,13 @@ JOIN Track AS t ON a.album_id = t.album_id
 GROUP BY album_title;
 
 --Все исполнители, которые не выпустили альбомы в 2020 году.
-SELECT name_pseudonym, album_title, year_of_release FROM Artist AS ar
-JOIN Artist_Album AS aa ON ar.artist_id = aa.artist_id  
-JOIN Album AS al ON aa.album_id = al.album_id 
-WHERE year_of_release <> 2020
-GROUP BY name_pseudonym, album_title, year_of_release
-ORDER BY year_of_release ASC;
+SELECT name_pseudonym FROM Artist
+WHERE name_pseudonym NOT IN (
+	SELECT name_pseudonym FROM Artist AS ar
+	JOIN Artist_Album AS aa ON ar.artist_id = aa.artist_id  
+	JOIN Album AS al ON aa.album_id = al.album_id 
+	WHERE year_of_release = 2020
+);
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 SELECT collection_title, name_pseudonym FROM collection AS c 
